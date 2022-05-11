7cf94ada36de06734bbebaa8abe2a4688319392d
Vũ Quang Duy <vduy2350@gmail.com>
2022-05-11 11:15:55 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  identity = "PH18088"
   title = 'WE16304_Angular';
-  name = 'Duyvqph18088';
+  name = 'Vũ Quang Duy';
   age = '18';
   students = [
     {
@@ -25,5 +26,31 @@ export class AppComponent {
       name: 'Student 3',
       status: 0
     },
+  ]
+  teachers = [
+    {
+      id: 'PH1234',
+      name: 'Teacher 1',
+      age: 20,
+      gender: 0,
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxekghgcizx9iOtfgMfM7Ln4keSFjAQk7trQ&usqp=CAU",
+      status: 0
+    },
+    {
+      id: 'PH2234',
+      name: 'Teacher 2',
+      age: 20,
+      gender: 1,
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxekghgcizx9iOtfgMfM7Ln4keSFjAQk7trQ&usqp=CAU",
+      status: 1
+    },
+    {
+      id: 'PH3234',
+      name: 'Teacher 3',
+      age: 20,
+      gender: 0,
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxekghgcizx9iOtfgMfM7Ln4keSFjAQk7trQ&usqp=CAU",
+      status: 0
+    }
   ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { TableNameComponent } from './table/table-name/table-name.component';
+import { TableGenderComponent } from './table/table-gender/table-gender.component';
+import { TableStatusComponent } from './table/table-status/table-status.component';
+import { NameComponent } from './name/name.component';
+import { IdentityComponent } from './identity/identity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    TableNameComponent,
+    TableGenderComponent,
+    TableStatusComponent,
+    NameComponent,
+    IdentityComponent
   ],
   imports: [
     BrowserModule,
