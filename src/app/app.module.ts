d909e51abec073c9bc70e7f58ef6d6c39b5fd4b9
Vũ Quang Duy <vduy2350@gmail.com>
2022-06-08 09:43:23 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShowValidateComponent } from './components/show-validate/show-validate.component';
-import { HeaderComponent } from './components/header/header.component';
 import { LayoutClientComponent } from './pages/layout/layout-client/layout-client.component';
 import { LayoutAdminComponent } from './pages/layout/layout-admin/layout-admin.component';
-import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { HeaderAdminComponent } from './components/admin/header-admin/header-admin.component';
 import { AdminProductListComponent } from './pages/admin-product/admin-product-list/admin-product-list.component';
 import { AdminProductFormComponent } from './pages/admin-product/admin-product-form/admin-product-form.component';
 import { AdminProductDetailComponent } from './pages/admin-product/admin-product-detail/admin-product-detail.component';
@@ -16,12 +15,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './pages/auth/login/login.component';
-import { RegisterComponent } from './pages/auth/register/register.component'
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { HeaderClientComponent } from './components/client/header-client/header-client.component';
+import { AsideAdminComponent } from './components/admin/aside-admin/aside-admin.component'
 @NgModule({
   declarations: [
     AppComponent,
     ShowValidateComponent,
-    HeaderComponent,
     LayoutClientComponent,
     LayoutAdminComponent,
     HeaderAdminComponent,
@@ -29,7 +29,9 @@ import { RegisterComponent } from './pages/auth/register/register.component'
     AdminProductFormComponent,
     AdminProductDetailComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderClientComponent,
+    AsideAdminComponent
   ],
   imports: [
     BrowserModule,
